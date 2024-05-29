79796fb6d9fc19e0d55d2b588305eab3bb548f86
KhoDis <khodisdev@gmail.com>
2024-05-29 22:54:48 +0300

    feat(client): add createTask.tsx

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -1,10 +1,12 @@
-import TaskList from '@/components/TaskList';
+import TaskList from '@/app/tasks/TaskList';
+import {CreateTask} from "@/app/tasks/createTask";
 
 const Tasks = () => {
   return (
     <div>
       <h1>Tasks</h1>
       <TaskList />
+      <CreateTask />
     </div>
   );
 };
